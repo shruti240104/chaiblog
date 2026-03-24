6dc232f2a0bdba7c45b86b46ed4d1cdff3a472a3
shruti240104 <shrutisingh240504@gmail.com>
2026-03-24 22:40:20 +0530

    feat(layout): add header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { useDispatch } from 'react-redux'
 import authService from "./appwrite/auth"
-import { login } from './store/authSlice'
+import { login,logout } from './store/authSlice'
 import { Footer, Header } from './components/index'
 import { Outlet } from 'react-router-dom'
 
