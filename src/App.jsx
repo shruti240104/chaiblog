b53e626afd55bc801c53b9f53e277f2541c8ce54
shruti240104 <shrutisingh240504@gmail.com>
2026-03-24 11:41:06 +0530

    feat(auth): add authentication reducers for login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import { login } from './store/authSlice'
+import { Footer, Header } from './components/index'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getCurrentUser().then((userData)=>{
+      if(userData){
+        dispatch(login(userData));
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO : {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
