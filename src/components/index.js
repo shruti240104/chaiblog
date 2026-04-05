35a1962aafab6572f84d9d914c5d4472da5173de
shruti240104 <shrutisingh240504@gmail.com>
2026-04-05 15:45:01 +0530

    feat(posts): add post creation form with rich text editor

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ import Footer from './footer/Footer';
 import Container from './container/Container';
 import Logo from './Logo';
 import LogoutBtn from './header/LogoutBtn';
+import RTE from './RTE';
 export {
   Header,
   Footer,
   Container,
   Logo,
-  LogoutBtn
+  LogoutBtn,
+  RTE
 }
